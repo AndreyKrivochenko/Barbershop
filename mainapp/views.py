ea7eef8b81eca2eb3b9f40bbe2e6c89b16ff6b33
Andrey Krivochenko <krivochenko_andrey@mail.ru>
2021-08-12 23:38:10 +0700

    Галерея. Контроллер + шаблон

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,9 +1,18 @@
 from django.http import HttpResponseNotFound
 from django.views.generic import TemplateView
 
+from mainapp.models import MainGallery
+
 
 class IndexView(TemplateView):
+    model = MainGallery
     template_name = 'mainapp/index.html'
+
+    def get_context_data(self, **kwargs):
+        data = super(IndexView, self).get_context_data(**kwargs)
+        gallery = MainGallery.objects.all()
+        data['gallery'] = gallery
+        return data
 
 
 class ContactView(TemplateView):
